c7a79154b6c9e60c7ba864e324c24a22cc0d4c62
radoslawbb <radbal2005@gmail.com>
2025-10-16 15:09:23 +0200

    lab1

--- gr2_PP_lab1/gr2_PP_lab1.cpp
+++ gr2_PP_lab1/gr2_PP_lab1.cpp
@@ -41,7 +41,7 @@ int main() {
     printf("    ***********************    \n");
     printf("      *******************      \n");
     printf("        ***************        \n");
-    printf("         ************          \n");
+    printf("         *************          \n");
     printf("        ***************        \n");
     printf("       *****        *****      \n");
     printf("      ***              ***     \n");
@@ -58,7 +58,7 @@ int main() {
     cout << "    ***********************    \n";
     cout << "      *******************      \n";
     cout << "        ***************        \n";
-    cout << "         ************          \n";
+    cout << "         *************          \n";
     cout << "        ***************        \n";
     cout << "       *****       *****      \n";
     cout << "      ***             ***     \n";
@@ -67,12 +67,89 @@ int main() {
 }*/
 
 //zadanie 3 printf
-int main() {
+/*int main() {
+    printf("     Lista obecnosci             \n");
+    printf("nr  |     imie     |     nazwisko\n");
+    printf("1   |   Radoslaw   |     Balinski\n");
+    printf("2   |    Tomasz    |     Iksinski\n");
+    printf("3   |     Anna     |     Igrekowa\n");
+
+    return 0;
+}*/
+
+//cout
+/*int main() {
+    cout << "     Lista obecnosci             \n";
+    cout << "nr  |     imie     |     nazwisko\n";
+    cout << "1   |   Radoslaw   |     Balinski\n";
+    cout << "2   |    Tomasz    |     Iksinski\n";
+    cout << "3   |     Anna     |     Igrekowa\n";
+
+    return 0;
+}*/
+
+//zadanie 4
+//printf scanf
+
+/*int main() {
+    int a, b, c, d;
+    printf("Wprowadz a: ");
+    scanf_s("%d", &a);
+    printf("\nWprowadz b: ");
+    scanf_s("%d", &b);
     
+    c = a * b;
+    d = a + b;
+
+    printf("\nWynik dodawania: %d, wynik mnozenia: %d\n", d, c);
+
+    return 0;
+}*/
+
+//cout cin
+
+/*int main() {
+    int a, b, c, d;
+    cout<<"Wprowadz a: ";
+    cin >> a;
+    cout<<"\nWprowadz b: ";
+    cin >> b;
+
+    c = a * b;
+    d = a + b;
+
+    cout << "\nWynik dodawania: " << d << ", wynik mnozenia: " << c;
+
+    return 0;
+}*/
+
+//zadanie 5
+
+//printf scanf
+
+/*#include <stdio.h>
+int main() {
+    int f, c;
+    printf("Wprowadz dowolna temperature w stopniach Fahrenheita: ");
+    scanf_s("%d", &f);
+    c = 5 * (f - 32) / 9;
+    printf("\n%d stopni Fahrenheita to %d stopni Celsjusza", f, c);
+
+    return 0;
+}*/
+
+//cout cin
+
+#include <stdio.h>
+int main() {
+    int f, c;
+    cout << "Wprowadz dowolna temperature w stopniach Fahrenheita: ";
+    cin >> f;
+    c = 5 * (f - 32) / 9;
+    cout << endl << f << " stopni Fahrenheita to " << c << " stopni Celsjusza";
 
     return 0;
 }
-
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
